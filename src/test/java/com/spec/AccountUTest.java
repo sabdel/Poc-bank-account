1acc7565bfd76f2730e9aa85f1658b6e27b17f48
Salaheddine <Salaheddine.Abdessaied@holydis.com>
2017-11-28 05:33:29 +0100

    Fix Withdraw from account feature "Some Unit Tests "

--- src/test/java/com/spec/AccountUTest.java
+++ src/test/java/com/spec/AccountUTest.java
@@ -5,19 +5,22 @@ import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+
 import com.bank.dao.Account;
+import com.bank.dao.StatementItem;
 import com.bank.services.ServiceAccount;
 import com.bank.services.ServiceAccountImpt;
 
+import junitparams.Parameters;
+
 @RunWith(JUnit4.class)
 public class AccountUTest {
-	
-
 
 	private ServiceAccount sAccount = new ServiceAccountImpt();
 
@@ -31,7 +34,7 @@ public class AccountUTest {
 		final BigDecimal aDepositOf100 = BigDecimal.valueOf(100.0);
 		final Account expectedAccount = sAccount.deposit(null, aDepositOf100, null);
 	}
- 
+
 	/**
 	 * should_fail_when_there_is_no_amount
 	 *
@@ -41,6 +44,7 @@ public class AccountUTest {
 		final Account account = new Account();
 		final Account expectedAccount = sAccount.deposit(account, null, null);
 	}
+
 	@Test
 	public void should_return_statement_balance_100_when_i_deposit_100() {
 		final BigDecimal aDepositOf100 = BigDecimal.valueOf(100.0);
@@ -62,4 +66,87 @@ public class AccountUTest {
 		assertNotNull(expectedAccount);
 		assertEquals(0, expectedAccount.getBalance().compareTo(aDepositOf100));
 	}
+
+	/**
+	 * should_return_statement_balance_50_when_i_deposit_100_and_i_withdraw_50.
+	 *
+	 */
+	@Test
+	public void should_return_statement_balance_50_when_i_deposit_100_and_i_withdraw_50() {
+		final Account account = new Account();
+		final BigDecimal aDepositOf100 = BigDecimal.valueOf(100.0);
+		final BigDecimal aWithdrawOf50 = BigDecimal.valueOf(50.0);
+
+		sAccount.deposit(account, aDepositOf100, null, LocalDateTime.now());
+		sAccount.withdraw(account, aWithdrawOf50, null, LocalDateTime.now());
+		assertEquals(0, (account.getBalance().compareTo(BigDecimal.valueOf(50))));
+	}
+
+	/**
+	 * should_return_statement_balance_minus_50_when_i_withdraw_50_and_200_as_overdraft.
+	 *
+	 */
+	@Test
+	public void should_return_statement_balance_minus_50_when_i_withdraw_50_and_200_as_overdraft() {
+		final Account account = new Account();
+		account.setOverdraft(BigDecimal.valueOf(200.0));
+		final BigDecimal aWithdrawOf50 = BigDecimal.valueOf(50.0);
+
+		sAccount.withdraw(account, aWithdrawOf50, null, LocalDateTime.now());
+		assertEquals(0, (account.getBalance().compareTo(BigDecimal.valueOf(-50))));
+	}
+
+
+	/**
+	 * should_return_statement_items_size_2_and_balance_50_when_i_deposit_100_and_i_withdraw_50.
+	 *
+	 */
+	@Test
+	public void should_return_statement_items_size_2_and_balance_50_when_i_deposit_100_and_i_withdraw_50() {
+		
+		final Account account = new Account();
+		final BigDecimal aDepositOf100 = BigDecimal.valueOf(100.0);
+		final BigDecimal aWithdrawOf50 = BigDecimal.valueOf(50.0);
+
+		sAccount.deposit(account, aDepositOf100, null, LocalDateTime.now());
+		sAccount.withdraw(account, aWithdrawOf50, null, LocalDateTime.now());
+
+		final List<StatementItem> statement = account.getStatements();
+		assertEquals(0, (account.getBalance().compareTo(BigDecimal.valueOf(50))));
+		assertEquals(2,statement.size());
+	}
+
+//	/**
+//	 * should_return_statement_balance_$1_when_i_did_transaction_with_amount_$2_$3_$4.
+//	 *
+//	 * @param expectedBalance
+//	 * @param amount1
+//	 * @param amount2
+//	 * @param amount3
+//	 */
+//	@Test
+//	@Parameters({ "50, 100, -30, -20", "40, 50, 0, -10", "0, 0, 0, 0", "30, 10, 10, 10"
+//
+//	})
+//	public void should_return_statement_balance_$1_when_i_did_transaction_with_amount_$2_$3_$4(
+//			final double expectedBalance, final BigDecimal amount1, final BigDecimal amount2, final BigDecimal amount3) {
+//		final Account account = new Account();
+//		if (amount1.compareTo(BigDecimal.ZERO)>0) {
+//			sAccount.deposit(account, amount1, null, LocalDateTime.now());
+//		} else {
+//			sAccount.withdraw(account, amount1.negate(), null, LocalDateTime.now());
+//		}
+//		if (amount2.compareTo(BigDecimal.ZERO)>0) {
+//			sAccount.deposit(account, amount2, null, LocalDateTime.now());
+//					} else {
+//						sAccount.withdraw(account, amount2.negate(), null, LocalDateTime.now());
+//		}
+//		if (amount3.compareTo(BigDecimal.ZERO)>0) {
+//			sAccount.deposit(account, amount3, null, LocalDateTime.now());
+//					} else {
+//						sAccount.withdraw(account, amount3.negate(), null, LocalDateTime.now());
+//		}
+//		assertEquals(0, (account.getBalance().compareTo(BigDecimal.valueOf(expectedBalance))));
+//	}
+
 }
