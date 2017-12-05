7d9e02d4fc61ec4079db223b8d0ada382a090f69
Salaheddine <Salaheddine.Abdessaied@holydis.com>
2017-12-05 05:38:01 +0100

    Fix show history feature

--- src/test/java/com/spec/steps/ShowHistoryAccountStepDefs.java
+++ src/test/java/com/spec/steps/ShowHistoryAccountStepDefs.java
@@ -1,15 +1,21 @@
 package com.spec.steps;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Currency;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.bank.domain.Account;
 import com.bank.services.ServiceAccount;
 import com.bank.services.ServiceAccountImpt;
 
+import cucumber.api.DataTable;
+import cucumber.api.Transformer;
 import cucumber.api.java8.En;
 
 public class ShowHistoryAccountStepDefs implements En {
@@ -18,24 +24,73 @@ public class ShowHistoryAccountStepDefs implements En {
 
 	private Account account = new Account();
 
+	private String history = "";
+
 	public ShowHistoryAccountStepDefs() {
 
-		Given("^I deposit (\\-?\\d+\\.\\d+) euros$", (BigDecimal amount) -> {
-			serviceAccount.deposit(account, amount, Currency.getInstance("EUR"), LocalDateTime.now());
+		// Given("^I deposit (\\d+) euros (.*) $", (BigDecimal
+		// amount,@Transform(ISO_LOCAL_DATE_TIMETimeConverter.class)
+		// LocalDateTime d) -> {
+		// serviceAccount.deposit(account, amount, Currency.getInstance("EUR"),
+		// d);
+		//
+		// });
+		//
+		// Given("^I withdraw (\\d+) euros (.*) $", (BigDecimal
+		// amount,@Transform(ISO_LOCAL_DATE_TIMETimeConverter.class)
+		// LocalDateTime d) -> {
+		// serviceAccount.withdraw(account, amount, null,d);
+		// });
 
+		// Given("^I deposit (\\d+) euros \"([^\"]*)\"$", (BigDecimal amount,
+		// @Format("yyyy-MM-dd'T'HH:mm:ss") LocalDateTime d) -> {
+		// serviceAccount.deposit(account, amount, Currency.getInstance("EUR"),
+		// d);
+		// });
+		//
+		// Given("^I withdraw (\\d+) euros \"([^\"]*)\"$", (BigDecimal amount,
+		// @Format("yyyy-MM-dd'T'HH:mm:ss") LocalDateTime d) -> {
+		// serviceAccount.withdraw(account, amount, null,d);
+		//
+		// });
+
+		Given("^I deposit (\\d+) euros  \"([^\"]*)\"$", (BigDecimal amount, String date) -> {
+			serviceAccount.deposit(account, amount, Currency.getInstance("EUR"),
+					LocalDateTime.parse(date, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 		});
 
-		Given("^I withdraw (\\-?\\d+\\.\\d+) euros$", (BigDecimal amount) -> {
-			serviceAccount.withdraw(account, amount, null);
+		Given("^I withdraw (\\d+) euros  \"([^\"]*)\"$", (BigDecimal amount, String date) -> {
+			serviceAccount.withdraw(account, amount, null,
+					LocalDateTime.parse(date, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
 		});
 
 		When("^I ask the statement$", () -> {
-			serviceAccount.print(account);
+			history = serviceAccount.print(account);
 		});
 
-		Then("^sMy balance should be (\\-?\\d+\\.\\d+)$", (BigDecimal newBalance) -> {
-			assertEquals(0, serviceAccount.getBalance(account).compareTo(newBalance));
+		Then("^history  should be$", (DataTable expectedStatementItems) -> {
+
+			String expectedHistory = expectedStatementItems.raw().stream()
+					.map(new Function<List<String>, String>() {
+						@Override
+						public String apply(List<String> statementItem) {
+							return statementItem.stream().map(s -> s.toString()).collect(Collectors.joining(" "));
+						}
+					}).collect(Collectors.joining("\n"));
+
+			assertThat(history).isEqualTo(expectedHistory);
+
 		});
 
 	}
+
+	public static class ISO_LOCAL_DATE_TIMETimeConverter extends Transformer<LocalDateTime> {
+
+		@Override
+		public LocalDateTime transform(String value) {
+			return LocalDateTime.parse(value, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+		}
+	}
+
 }
--- src/test/resources/com/spec/features/account_history.feature
+++ src/test/resources/com/spec/features/account_history.feature
@@ -3,10 +3,14 @@ As a bank client
 I want to see the history (operation, date, amount, balance) of my operations
 
 
-Scenario: Multiple Deposit  Withdraw and ask the statement balance
+Scenario: Multiple Deposit  Withdraw and show history 
     
-    Given I deposit 100.0 euros
-    And I withdraw 50.0 euros
+    Given I deposit 100 euros  "2000-01-01T00:00:00"
+    And   I withdraw 50 euros  "2000-01-02T00:00:00"
     When I ask the statement
-    Then sMy balance should be 50.0
+    Then history  should be 
+    
+    |2000-01-01T00:00:00| DEPOSIT   | 100.0 | 100.0 |
+    |2000-01-02T00:00:00| WITHDRAWAL| 50.0  | 50.0  |
+    
     
